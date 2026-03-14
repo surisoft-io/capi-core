94e9d47cbf9e81e2f8a93f80cb3b8efb79ca67b3
rodrigoserracoelho <rodrigoserracoelho@gmail.com>
2026-03-14 12:37:24 +0100

    Enabling metrics for platform vs virtual

--- src/main/java/io/surisoft/capi/configuration/MetricsConfiguration.java
+++ src/main/java/io/surisoft/capi/configuration/MetricsConfiguration.java
@@ -1,6 +1,7 @@
 package io.surisoft.capi.configuration;
 
 import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
@@ -58,6 +59,21 @@ public class MetricsConfiguration {
         }
         new JvmThreadMetrics().bindTo(prometheus);
         new ProcessorMetrics().bindTo(prometheus);
+
+        // Platform vs virtual thread breakdown (Java 21+)
+        Gauge.builder("jvm_threads_platform", () ->
+                Thread.getAllStackTraces().keySet().stream()
+                        .filter(t -> !t.isVirtual())
+                        .count()
+        ).description("The current number of platform (non-virtual) threads")
+         .register(prometheus);
+
+        Gauge.builder("jvm_threads_virtual", () ->
+                Thread.getAllStackTraces().keySet().stream()
+                        .filter(Thread::isVirtual)
+                        .count()
+        ).description("The current number of virtual threads")
+         .register(prometheus);
 
         compositeMeterRegistry.add(prometheus);
         return compositeMeterRegistry;
